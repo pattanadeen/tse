d0dc03d66ed9054fe79b6c9ccf7df9243f71be63
minchayisara <chayisara.sakunkoo.ug@dartmouth.edu>
2021-11-01 16:12:10 -0400

    final eiei

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -15,10 +15,10 @@ int sum;
 //  *
 //  *  Step 3
 //  *
-typedef struct word {
-	char *word;
-	int count;
-} word_t;
+// typedef struct word {
+// 	char *word;
+// 	int count;
+// } word_t;
 //  *
 //  *  Step 3
 //  *
@@ -27,10 +27,10 @@ typedef struct word {
 // *
 // * Step 4,5,6,7
 // *
-// typedef struct word {
-// 	char *word;
-// 	queue_t *qdocument;
-// } word_t;
+typedef struct word {
+	char *word;
+	queue_t *qdocument;
+} word_t;
 // *
 // * Step 4,5,6,7
 // *
@@ -78,15 +78,15 @@ bool search_doc(void* elementp, const void* keyp){
 //  *
 //  *  Step 3
 //  *
-void sumwords(void *elementp){
-    word_t *wordp = (word_t*) elementp;
-    sum = sum + wordp->count;
-}
- void free_word(void *elementp){
-    word_t *wordp = (word_t*) elementp;
+// void sumwords(void *elementp){
+//     word_t *wordp = (word_t*) elementp;
+//     sum = sum + wordp->count;
+// }
+//  void free_word(void *elementp){
+//     word_t *wordp = (word_t*) elementp;
 
-    free(wordp->word);
-}
+//     free(wordp->word);
+// }
 //  *
 //  *  Step 3
 //  *
@@ -99,17 +99,17 @@ void qsumwords(void *elementp) {
 // *
 // * Step 4,5,6,7
 // *
-// void sumwords(void *elementp){
-//     word_t *wordp = (word_t*) elementp;
-//     qapply(wordp->qdocument, qsumwords);
-// }
+void sumwords(void *elementp){
+    word_t *wordp = (word_t*) elementp;
+    qapply(wordp->qdocument, qsumwords);
+}
 
-// void free_word(void *elementp){
-//     word_t *wordp = (word_t*) elementp;
+void free_word(void *elementp){
+    word_t *wordp = (word_t*) elementp;
 
-//     free(wordp->word);
-//     qclose(wordp->qdocument);
-// }
+    free(wordp->word);
+    qclose(wordp->qdocument);
+}
 // *
 // * Step 4,5,6,7
 // *
@@ -139,39 +139,39 @@ int main(int argc, char *argv[]) {
     // *
     //  *  Step 3
     //  *
-    hashtable_t *htp = hopen(10);
-    webpage_t *pagep = pageload(1, "../pages-depth3/");
-    char *word;
-    int pos = 0;
+    // hashtable_t *htp = hopen(10);
+    // webpage_t *pagep = pageload(1, "../pages-depth3/");
+    // char *word;
+    // int pos = 0;
 
-    while ((pos = webpage_getNextWord(pagep, pos, &word)) > 0){
-        if(NormalizeWord(word) == 0){
-            // printf("%s\n", word);
-            word_t *words = hsearch(htp, search_word, word, strlen(word));
+    // while ((pos = webpage_getNextWord(pagep, pos, &word)) > 0){
+    //     if(NormalizeWord(word) == 0){
+    //         // printf("%s\n", word);
+    //         word_t *words = hsearch(htp, search_word, word, strlen(word));
 
-            if(words != NULL){
-                words->count = words->count +1;
-                free(word);
-            }
-            else{
-                word_t *wordp = malloc(sizeof(word_t));
-                wordp->word = word;
-                wordp->count = 1;
-                hput(htp, (void *) wordp, word, strlen(word));
-            }
-        } else {
-            free(word);
-        }
-    }
+    //         if(words != NULL){
+    //             words->count = words->count +1;
+    //             free(word);
+    //         }
+    //         else{
+    //             word_t *wordp = malloc(sizeof(word_t));
+    //             wordp->word = word;
+    //             wordp->count = 1;
+    //             hput(htp, (void *) wordp, word, strlen(word));
+    //         }
+    //     } else {
+    //         free(word);
+    //     }
+    // }
     
-    sum = 0;
-    happly(htp, sumwords);
-    printf("%d", sum);
+    // sum = 0;
+    // happly(htp, sumwords);
+    // printf("%d", sum);
 
-    happly(htp, free_word);
-    hclose(htp);
+    // happly(htp, free_word);
+    // hclose(htp);
 
-    exit(EXIT_SUCCESS);
+    // exit(EXIT_SUCCESS);
     //  *
     //  *  Step 3
     //  *
@@ -245,85 +245,91 @@ int main(int argc, char *argv[]) {
 //     *
 //     *  Step 7
 //     *
-//    if(argc!=3 || argv[1] == NULL || argv[2] == NULL){
-//         printf("usage: indexer <pagedir> <indexnm>\n");
-//         exit(EXIT_FAILURE);
-//     }
+   if(argc!=3 || argv[1] == NULL || argv[2] == NULL){
+        printf("usage: indexer <pagedir> <indexnm>\n");
+        exit(EXIT_FAILURE);
+    }
 
-//     char *pagedir_temp = argv[1];
-//     char *indexnm = argv[2];
-//     char pagedir[16];
-//     sprintf(pagedir, "%s%s%s", "../", pagedir_temp, "/");
-//     printf("%s\n", pagedir);
-//     printf("%s\n", indexnm);
+    char *pagedir_temp = argv[1];
+    char *indexnm = argv[2];
+    char pagedir[16];
+    sprintf(pagedir, "%s%s%s", "../", pagedir_temp, "/");
+    printf("%s\n", pagedir);
+    printf("%s\n", indexnm);
     
-//     hashtable_t *htp = hopen(10);
+    hashtable_t *htp = hopen(10);
 
-//     int count = 0;
-//     DIR *d;
-//     struct dirent *dir;
-//     d = opendir(pagedir);
+    int count = 0;
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(pagedir);
 
-//     if (d) {
-//         printf("exist");
-//     } else if (ENOENT == errno) {
-//         printf("not exist");
-//         return -2;
-//     } else {
-//         return -1;
-//         printf("failed exist");
-//     }
+    if (d) {
+        printf("exist");
+    } else if (ENOENT == errno) {
+        printf("not exist");
+        return -2;
+    } else {
+        return -1;
+        printf("failed exist");
+    }
 
-//     if (d){
-//         while ((dir = readdir(d)) != NULL){
-//             printf("%s\n", dir->d_name);
-//             count++;
-//             char *ptr;
-//             int i = strtol(dir->d_name, &ptr, 10);
-//             printf("%d\n", i);
+    if(d){
+        while ((dir = readdir(d)) != NULL){
+            count++;
+        }
+    }
+    count = count - 2;
+    int i = 1;
 
-//             webpage_t *pagep = pageload(i, pagedir);
-//             char *word;
-//             int pos = 0;
+    if (d){
+        while (i != count+1){
+            // printf("%s\n", dir->d_name);
+            printf("%d\n", i);
 
-//             while ((pos = webpage_getNextWord(pagep, pos, &word)) > 0){
-//                 if(NormalizeWord(word) == 0){
-//                     word_t *words = hsearch(htp, search_word, word, strlen(word));
-//                     doc_t *document;
-//                     if(words != NULL){
-//                         if ((document = (doc_t *) qsearch(words->qdocument, search_doc, &i)) != NULL) {
-//                             document->count = document->count + 1;
-//                         }
-//                         else {
-//                             qput(words->qdocument, make_doc(i, 1));
-//                         }
-//                         free(word);
-//                     }
-//                     else{
-//                         word_t *wordp = malloc(sizeof(word_t));
-//                         wordp->word = word;
-//                         wordp->qdocument = qopen();
+            webpage_t *pagep = pageload(i, pagedir);
+            char *word;
+            int pos = 0;
 
-//                         qput(wordp->qdocument, make_doc(i, 1));
-//                         hput(htp, (void *) wordp, word, strlen(word));
-//                     }
-//                 } else {
-//                     free(word);
-//                 }
-//             }
-//             webpage_delete(pagep);
-//             free(pagep);
-//             free(word);
-//         }
-//             closedir(d); 
-//     }   
+            while ((pos = webpage_getNextWord(pagep, pos, &word)) > 0){
+                if(NormalizeWord(word) == 0){
+                    word_t *words = hsearch(htp, search_word, word, strlen(word));
+                    doc_t *document;
+                    if(words != NULL){
+                        if ((document = (doc_t *) qsearch(words->qdocument, search_doc, &i)) != NULL) {
+                            document->count = document->count + 1;
+                        }
+                        else {
+                            qput(words->qdocument, make_doc(i, 1));
+                        }
+                        free(word);
+                    }
+                    else{
+                        word_t *wordp = malloc(sizeof(word_t));
+                        wordp->word = word;
+                        wordp->qdocument = qopen();
 
-//     indexsave(htp, indexnm, "../indices/");
+                        qput(wordp->qdocument, make_doc(i, 1));
+                        hput(htp, (void *) wordp, word, strlen(word));
+                    }
+                } else {
+                    free(word);
+                }
+            }
+            webpage_delete(pagep);
+            free(pagep);
+            free(word);
+            i++;
+        }
+            closedir(d); 
+    }   
 
-//     happly(htp, free_word);
-//     hclose(htp);
+    indexsave(htp, indexnm, "../indices/");
 
-//     exit(EXIT_SUCCESS);
+    happly(htp, free_word);
+    hclose(htp);
+
+    exit(EXIT_SUCCESS);
 //     *
 //     *  Step 7
 //     *
